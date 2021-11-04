3ca87246f5e2f7fa07eb0d44add6d5b844b6a698
Luis Diaz <ldiazn98@gmail.com>
2021-11-04 18:07:44 -0400

    added some filtering widgets

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
     State for the application, computing the data to show
 """
 # Local imports
+from typing import List
 import c4v.microscope as ms
 import config         as c
 
@@ -10,11 +11,17 @@ import logging as log
 
 # Third party imports
 import pandas as pd
-
+import streamlit as sl
 class App:
     """
         Main application object, mainly used to get the data to show in the front end
     """
+
+    # Valid options for labels
+    label_options : List[str] = ["ANY", "IRRELEVANTE", "PROBLEMA DEL SERVICIO", "NO LABEL"]
+
+    # Valid options for scraped
+    scraped_options : List[str] = ["Any","Yes", "No"]
 
     def __init__(self, config : c.Config = c.Config()) -> None:
         # create persistency manager based on the specified backend
@@ -23,13 +30,55 @@ class App:
         else:
             raise NotImplementedError(f"DB Backend '{config.db_backend}' not yet implemented")
 
-    def get_dashboard_data(self, max_rows : int = 100) -> pd.DataFrame:
+    @sl.cache
+    def get_dashboard_data(self, max_rows : int = 100, max_content_len : int = 200, label : str = "ANY", scraped : str = "Any") -> pd.DataFrame:
         """
             Get data to show in the dashboard
             # Parameters:   
                 - max_rows : `int` = (optional) maximum amount of rows to display
+                - max_content_len : `int` = (optional) maximum amount of chars to show in the content field which might be long
+                - label : `str` = (optional) description of the label that every instance should have
+                - scraped : `str` = (optional) if the instances should be scraped. 
         """
-        return pd.DataFrame(list(self._manager.get_all(limit=max_rows)))
+        # Some sanity check
+        assert label in App.label_options, f"invalid label: {label}"
+        assert scraped in App.scraped_options, f"invalid scraped option: {scraped}"
+
+        # get value depending on if the instances should be scraped
+        opt_2_val = {
+            "Any" : None,
+            "Yes" : True,
+            "No"  : False
+        }
+
+        query = self._manager.get_all(scraped=opt_2_val[scraped])
+
+        # Add filtering
+        if label == "NO LABEL":
+            query = (x for x in query if not x.label)
+        elif label != "ANY":
+            query = (x for x in query if x.label and x.label.value == label)
+
+
+        elems = []
+        for d in query:
+            
+            # Reformat enum fields
+            if d.source: d.source = d.source.value
+            if d.label:  d.label  = d.label.value
+
+            # truncate body if needed
+            content_len = len(d.content)
+            d.content = d.content if content_len < max_content_len else d.content[:max_content_len] + "..."
+            elems.append(d)
+
+            # break if gathered enough rows
+            if len(elems) == max_rows:
+                break
+
+            
+
+        return pd.DataFrame(elems)
 
         
 
--- main.py
+++ main.py
@@ -7,6 +7,22 @@ import app
 sl.write("# 🔬 C4V Microscope")
 sl.write("In the following dashboard we will see how the scraping, crawling and classification works.")
 
-app_instance = app.App()
+app = app.App()
 
-sl.write("hellooooooo")
+# Add a title to the sidebar
+sl.sidebar.write("## Filters")
+sl.sidebar.write("Specify which data you want to see with the following filters")
+
+# Add a selector to filter by label 
+label = sl.sidebar.selectbox("Label: ", options=app.label_options, help="Label assigned by the classifier")
+scraped = sl.sidebar.selectbox("Scraped: ", options=app.scraped_options, help="Whether the instance data is complete or not")
+use_max_rows = sl.sidebar.checkbox("Limit Rows: ", value=True, help="Max ammount of rows to show")\
+
+# If limiting the max amount of rows, then ask for a max value
+if use_max_rows:
+    max_rows = sl.sidebar.number_input("Max: ", 0, value=100, help="Maximum amount of rows to retrieve and show")
+else:
+    max_rows = -1
+
+sl.dataframe(app.get_dashboard_data(label=label, max_rows=max_rows))
+
