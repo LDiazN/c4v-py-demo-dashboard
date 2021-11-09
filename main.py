81591faee88959d123e4596a0323dbe8091f5a71
Luis Diaz <ldiazn98@gmail.com>
2021-11-09 15:38:35 -0400

    added crawling controllers

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 """
     Entry point for the streamlit app
 """
+import dataclasses
+from datetime import time
+from typing import List
+from PIL.Image import LIBIMAGEQUANT
 import streamlit as sl
 import app
 
@@ -58,6 +62,7 @@ def run_classification_callback():
         sl.info("Running classification process, this may take a while...")
         try:
             app.classify(branch_name, experiment_name, max_rows_to_classify)
+            sl.success("Classification finished")
         except Exception as e:
             sl.error(   f"Unable to classify using model {branch_name}/{experiment_name} {('and up to ' + str(max_rows_to_classify)) if max_rows_to_classify >= 0 else ''}.    " + \
                         f"Error: {e}"
@@ -65,6 +70,73 @@ def run_classification_callback():
 
 sl.sidebar.button("Classify", help="Perform the classification process", on_click=run_classification_callback)
 
+# -- < Crawling Controls >  -----------------------------------
+sl.sidebar.write("------")
+sl.sidebar.write("## Crawling")
+sl.sidebar.write("Crawl for new urls in the available sites")
+
+# Limit
+use_crawl_limit = sl.sidebar.checkbox("Limit", help="Use a max amount of urls to be crawled at the same time", value=True)
+if use_crawl_limit:
+    crawl_limit = sl.sidebar.number_input("Max instances to add", 0, value=100)
+else:
+    crawl_limit = -1
+# Crawlers to use
+crawlers = sl.sidebar.multiselect("Crawl for: ", options=app.manager.get_available_crawlers(), help="Sites that can be scraped for new data")
+
+# utility class to keep count of how much instance were crawled so far
+@dataclasses.dataclass
+class Count:
+    count : int = 0
+
+    def add(self, additional : int):
+        self.count += additional
+
+crawled_so_far = Count()
+
+# Function to print a message with the current amount of crawled urls
+def generate_progress_infinite():
+
+    place_holder_msg = sl.empty()
+    def progress_infinite(urls : List[str]):
+        # Display some text telling how much urls have been crawled so far
+        crawled_so_far.add(len(urls))
+        place_holder_msg.info(f"Crawled {crawled_so_far.count} urls...")
+
+    return progress_infinite
+
+# Function to generate a finite progress bar function
+def generate_progress_finite():
+    sl.write("Progress")
+    progress_bar = sl.progress( 0 )
+    def progress_finite(urls : List[str]):
+        crawled_so_far.add(len(urls))
+        progress_bar.progress(int(100 * (crawled_so_far.count/crawl_limit)))
+        
+    return progress_finite
+
+def run_crawl_callback():
+    """
+        Run a classification and show an info tect 
+    """
+    # If nothing to do, just end the process
+    if not crawlers or crawl_limit == 0: return
+
+    # Start classification process
+    sl.info("Starting crawling process")
+    app.crawl(crawlers, crawl_limit, generate_progress_finite() if crawl_limit > 0 else generate_progress_infinite())
+
+    # Tell the user how much urls were crawled
+    if crawled_so_far.count == 0:
+        sl.warning("No urls were crawled. Maybe there were no urls to crawl?")
+        return
+
+    sl.success(f"Succesfully crawled {crawled_so_far.count} elements!")
+
+# Button to run
+sl.sidebar.button("Crawl", help="Run a crawling limit with the provided limits", on_click=run_crawl_callback)
+        
+
 # -- < Show Dashboard > ---------------------------------------
-sl.dataframe(app.get_dashboard_data(label=label, max_rows=max_rows))
+sl.dataframe(app.get_dashboard_data(label=label, max_rows=max_rows, scraped=scraped))
 
